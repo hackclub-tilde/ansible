068cbe6d3884b2e842e491ce050078d9003f0d0a
Ubergeek <ubergeek@yourtilde.com>
2019-02-10 21:43:13 +0000

    Fixing some perms, and the start script

--- roles/shell/files/var/lib/minecraft/paper/start.sh
+++ roles/shell/files/var/lib/minecraft/paper/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd /var/lib/minecraft/paper
 
 /usr/bin/java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar /var/lib/minecraft/paper/paperclip.jar
